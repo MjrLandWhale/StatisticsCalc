8e45ce26aebad0af2a3be4e8905ce2d489725830
b-swift <brad.alan.ewing@gmail.com>
2017-09-30 12:26:20 -0500

    Improved mode_test

--- mode.py
+++ mode.py
@@ -3,18 +3,19 @@ class ModeCalc(object):
         mode = []
         maxCount = 0
 
+        #finds the maximum occurrence of repeated numbers in the list
         for i in range(len(lst)):
             count = lst.count(lst[i])
             if count > maxCount:
                maxCount = count
 
+        #finds all numbers that occur as many times as the max occurrence
         for i in range(len(lst)):
             if lst.count(lst[i]) == maxCount:
                 mode.append(lst[i])
         return list(set(mode))
 
-
-lst = [1,3,2,1,3,2,1,4,1,2,2,2,2,3,7,8,3,9,5,3,4,3,8,3,2,2]
+lst = [3]
 mode = ModeCalc()
-print mode.calculate_mode(lst)
+#print mode.calculate_mode(lst)
 
--- mode_test.py
+++ mode_test.py
@@ -1,13 +1,55 @@
 import unittest
 from mode import ModeCalc
 
+#test where mode is a single number
 class Test1(unittest.TestCase):
 	def test_mode(self):
 		mode = ModeCalc()
-		input_a = [4, 3, 2, 3, 2, 3]
-		expected_output = 3
-		actual_output = ModeCalc.calculate_mode(mode, input_a)
-		self.assertEqual(expected_output, actual_output, 'mode = %d' % (actual_output))
+
+		input1 = [4, 3, 2, 3, 2, 3]
+		expected_output1 = [3]
+		actual_output1 = ModeCalc.calculate_mode(mode, input1)
+		self.assertEqual(expected_output1, actual_output1, 'mode = 3')
+
+#test where mode is two numbers
+class Test2(unittest.TestCase):
+	def test_mode2(self):
+		mode = ModeCalc()
+
+		input2 = [2,3,4,1,6,4,7,2]
+		expected_output2 = [2,4]
+		actual_output2 = ModeCalc.calculate_mode(mode, input2)
+		self.assertEqual(expected_output2, actual_output2, 'mode = 2, 4')
+
+#test where mode is three numbers
+class Test3(unittest.TestCase):
+	def test_mode3(self):
+		mode = ModeCalc()
+
+		input3 = [2, 3, 4, 1, 6, 4, 7, 2, 9, 1]
+		expected_output3 = [1, 2, 4]
+		actual_output3 = ModeCalc.calculate_mode(mode, input3)
+		self.assertEqual(expected_output3, actual_output3, 'mode = 1, 2, 4')
+
+#test where all numbers occur once
+class Test4(unittest.TestCase):
+	def test_mode4(self):
+		mode = ModeCalc()
+
+		input4 = [1, 2, 3, 4]
+		expected_output4 = [1, 2, 3, 4]
+		actual_output4 = ModeCalc.calculate_mode(mode, input4)
+		self.assertEqual(expected_output4, actual_output4, 'mode = 1, 2, 3, 4')
+
+#test will there is only one number
+class Test5(unittest.TestCase):
+	def test_mode5(self):
+		mode = ModeCalc()
+
+		input5 = [2]
+		expected_output5 = [2]
+		actual_output5 = ModeCalc.calculate_mode(mode, input5)
+		self.assertEqual(expected_output5, actual_output5, 'mode = 2')
 
 if __name__ == '__main__':
 	unittest.main()
