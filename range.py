322630b055b548137675db8631ceb897a5a54c29
Kaleigh Key <kaleigh.key@pioneerrx.com>
2017-10-06 00:15:01 -0500

    Calculating range works for a list of numbers (list length > 1)

--- range.py
+++ range.py
@@ -1,10 +1,16 @@
 class RangeCalc(object):
     def calculate_range(self, number_list):
-        range = []
+        calculated_range = None
 
-        return range
+        if len(number_list) > 1:
+            sorted_list = sorted(number_list)
+            smallest_num = sorted_list[0]
+            greatest_num = sorted_list[len(sorted_list) - 1]
+            calculated_range = greatest_num - smallest_num
 
-#number_list = [3]
-#range = RangeCalc()
-#print range.calculate_range(list)
+        return calculated_range
+
+# number_list = [2, 3, 5, 7]
+# range_num = RangeCalc()
+# print range_num.calculate_range(number_list)
 
--- range_test.py
+++ range_test.py
@@ -2,15 +2,15 @@ import unittest
 from range import RangeCalc
 
 
-#test for range
-class rangeTest(unittest.TestCase):
+# test for range
+class TestRange(unittest.TestCase):
 	def test_range(self):
-		range = RangeCalc()
+		range_num = RangeCalc()
 
 		number_list = [10, 6, 4, 2, 4, 7] # list of numbers for test
 
 		expected_output = 8 # expected output for number_list
-		actual_output = RangeCalc.calculate_range(range, number_list) # calculated value for number_list
+		actual_output = RangeCalc.calculate_range(range_num, number_list) # calculated value for number_list
 
 		self.assertEqual(expected_output, actual_output, 'range = %d' % actual_output)
 
