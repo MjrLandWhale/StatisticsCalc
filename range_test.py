6b3d8a6aefcab4f62769c4e024b8e9206c1fa5fa
Kaleigh Key <kaleigh.key@pioneerrx.com>
2017-10-06 00:19:21 -0500

    Added test for calculating range when there is only one number in the list

--- range_test.py
+++ range_test.py
@@ -2,8 +2,8 @@ import unittest
 from range import RangeCalc
 
 
-# test for range
-class TestRange(unittest.TestCase):
+# test for range with list of numbers
+class TestRangeList(unittest.TestCase):
 	def test_range(self):
 		range_num = RangeCalc()
 
@@ -13,6 +13,19 @@ class TestRange(unittest.TestCase):
 		actual_output = RangeCalc.calculate_range(range_num, number_list) # calculated value for number_list
 
 		self.assertEqual(expected_output, actual_output, 'range = %d' % actual_output)
+
+
+# test for range with one number in list
+class TestRangeOneNum(unittest.TestCase):
+	def test_range(self):
+		range_num = RangeCalc()
+
+		number_list = [7] # list of numbers for test
+
+		expected_output = None # expected output for number_list
+		actual_output = RangeCalc.calculate_range(range_num, number_list) # calculated value for number_list
+
+		self.assertEqual(expected_output, actual_output, 'range = %s' % actual_output)
 
 if __name__ == '__main__':
 	unittest.main()
