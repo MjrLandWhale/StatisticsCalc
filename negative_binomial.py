d799e5f729e81a83b3ac98f672c42e86e9f44a5f
b-swift <brad.alan.ewing@gmail.com>
2017-10-13 18:52:14 -0500

    Added negative_binomial.

--- negative_binomial.py
+++ negative_binomial.py
@@ -1,6 +1,16 @@
 import math
 from combination import CombinationCalc
 
+#returns the probability of having a certain number of failures before you have a certain number of successes
 class NegativeBinomialDist(object):
-    def neg_binomial_calc(self, success_prob, trials, successes):
-        pass
+    def neg_binomial_calc(self, success_prob, failures, successes):
+        comb = CombinationCalc()
+        if success_prob < 0 or failures < 0 or successes < 0:
+            return 'None of the numbers can be negative.'
+        elif success_prob == 0 or successes == 0:
+            return 'Successes and success_prob cannot be zero.'
+        else:
+            return round(comb.calc_combination(failures + successes - 1, successes - 1) * math.pow(success_prob, successes) * math.pow(1 - success_prob, failures), 4)
+
+#neg = NegativeBinomialDist()
+#print neg.neg_binomial_calc(0.7, 0, 6)
