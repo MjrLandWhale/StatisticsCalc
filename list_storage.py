e99e91b6135612862c67b351bd541fe4fb9a001d
MjrLandWhale <brandonemitch@gmail.com>
2017-10-17 19:14:56 -0500

    Completed list storage and added conditions to handle { and } characters in list_parser

--- list_storage.py
+++ list_storage.py
@@ -1,16 +1,11 @@
 class list_storage(object):
 
-    #Lists are stored as tuples in (name, list[]) format
-#    stored_lists = []
 
     #lets try a dict
     stored_lists = dict()
 
     #Store a list coming in from command line.  The list should already have been converted to integers at this point
     def store_list(self, name, list_to_store, DEBUG=False):
-
-        #Due to expected scope of this class, we do not need to sort stored_lists
- #       self.stored_lists.append( ( name, list_to_store ) )
 
         self.stored_lists[name] = list_to_store
 
@@ -22,13 +17,3 @@ class list_storage(object):
         except:
             #on missing key an exception is thrown
             return None
-
-
-        #Iterate through current stored lists in search of the correct name
-#        for i in self.stored_lists:
-
-            #If the stored name is equivalent to the requested name
-##               return i[1]
-
-        #If we reach this point that means the list isn't in storage, so return none
-#        return None
